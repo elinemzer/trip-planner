a4afc9ef3d056bf10f5ced3129f4c7d09757d5f3
Eli Nemzer <elinemzer7@gmail.com>
2017-05-05 14:33:42 -0400

    saved now

--- models/index.js
+++ models/index.js
@@ -1,6 +1,74 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/trip-planner');
 
+const Place = db.define('place', {
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  city: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  location: {
+    type: Sequelize.,
+    allowNull: false
+  }
+})
+
+const Hotel = db.define('hotel', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  num_stars:{
+    type: Sequelize.FLOAT(1,5),
+    allowNull: false
+  },
+  amenities: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
+
+const Activity = db.define('activity', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age_range: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
+
+const Restaurant = db.define('restaurant', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  cuisine: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.RANGE(Sequelize.INTEGER),
+    allowNull: false
+  }
+})
+
+// const Associations = db.define('associations', {
+//
+// })
+
 module.exports = {
   db: db
 }
